56d87cffd274fd8dbb50ddd36b28eaad09be883c
Ronny <kostmann.b@gmx.at>
2024-11-07 10:52:20 +0100

    started writing code and deleted vscode from gitignore folder

---
+++ src/at/kos/projects/camera/test/CameraTest.java
@@ -0,0 +1,84 @@
+package at.kos.projects.camera.test;
+
+import org.junit.Test;
+
+import at.kos.projects.camera.Camera;
+import at.kos.projects.camera.SDCard;
+
+public class CameraTest {
+    @Test
+    public void testGetAllFiles() {
+        SDCard sdCard = new SDCard(32800);
+        Camera cam = new Camera(48, 180, "green", null, null, null);
+    }
+
+    @Test
+    public void testGetAmountOfAllFiles() {
+
+    }
+
+    @Test
+    public void testGetColor() {
+
+    }
+
+    @Test
+    public void testGetFreeCapazity() {
+
+    }
+
+    @Test
+    public void testGetObjective() {
+
+    }
+
+    @Test
+    public void testGetPixel() {
+
+    }
+
+    @Test
+    public void testGetProducer() {
+
+    }
+
+    @Test
+    public void testGetResolution() {
+
+    }
+
+    @Test
+    public void testGetSdCard() {
+
+    }
+
+    @Test
+    public void testGetSystemCapazity() {
+
+    }
+
+    @Test
+    public void testGetWeight() {
+
+    }
+
+    @Test
+    public void testSetObjective() {
+
+    }
+
+    @Test
+    public void testSetResolution() {
+
+    }
+
+    @Test
+    public void testSetSdCard() {
+
+    }
+
+    @Test
+    public void testTakePicture() {
+
+    }
+}
